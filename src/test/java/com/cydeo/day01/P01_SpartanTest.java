7e35e314a83d2c74e3fb3fa29a4bcb547b5b4ce7
sunisaservais <sunisaservais@gmaill.com>
2025-07-02 18:20:12 -0400

    updated

--- src/test/java/com/cydeo/day01/P01_SpartanTest.java
+++ src/test/java/com/cydeo/day01/P01_SpartanTest.java
@@ -2,30 +2,18 @@ package com.cydeo.day01;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class P01_SpartanTest {
 
-    String spartanBaseUrl = "http://3.94.120.82:8000";
+    String baseUrl = "http://cydeo.onthewifi.com:8000/";
 
     @Test
     public void getAllSpartans() {
-        //send request spartan url and get response as Response interface
-        Response response = RestAssured.get(spartanBaseUrl+"/api/v2/spartans");
 
-        //both methods do the same thing
-        System.out.println("response.statusCode() = " + response.statusCode());
-        System.out.println("response.getStatusCode() = " + response.getStatusCode());
-
-        //verify that status code is 200
-        int actualStatusCode = response.statusCode();
-
-        Assertions.assertEquals(200, actualStatusCode);
-
-        //printing the json response body on the console
-        response.prettyPrint();
-//        response.print();
+        //Send request spartan url and get response as Response interface
+        Response response = RestAssured.get(baseUrl + "api/v2/spartans");
+        System.out.println("Status Code: " + response.statusCode());
 
     }
 }
